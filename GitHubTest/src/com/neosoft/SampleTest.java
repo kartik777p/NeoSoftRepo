5f0b81073f40120e25227379288da0081e5913ec
kartik777p <kartik777p@gmail.com>
2021-09-16 12:40:02 +0530

    3rd

--- GitHubTest/src/com/neosoft/SampleTest.java
+++ GitHubTest/src/com/neosoft/SampleTest.java
@@ -5,7 +5,7 @@ public class SampleTest {
 	public static void main(String[] args) {
 		String s1="SE";
 		
-		System.out.println("hiii ");
+		System.out.println("hiiii ");
 		if(s1.equalsIgnoreCase("se")) {
 			System.out.println(" your a Soft Engg");
 		}
