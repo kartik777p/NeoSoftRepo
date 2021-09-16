8db8473ecaa011316fc83acbc856c1bd61e29006
kartik777p <kartik777p@gmail.com>
2021-09-16 12:36:51 +0530

    2nd commit

--- GitHubTest/src/com/neosoft/SampleTest.java
+++ GitHubTest/src/com/neosoft/SampleTest.java
@@ -5,6 +5,7 @@ public class SampleTest {
 	public static void main(String[] args) {
 		String s1="SE";
 		
+		System.out.println("hiii ");
 		if(s1.equalsIgnoreCase("se")) {
 			System.out.println(" your a Soft Engg");
 		}
